16749d2804c2f070a0c94a9783fb63c7ace29100
ericdoppelt <ead45@duke.edu>
2021-10-14 21:27:56 -0400

    Created skeleton for UI using React and Material UI

---
+++ ui/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogoImage from '../images/logo.png'
+
+function Logo() {
+
+  return (
+    <div className='logo'>
+        <img height='30' width='auto' src={LogoImage}/>
+    </div>
+  );
+}
+
+export default Logo;
