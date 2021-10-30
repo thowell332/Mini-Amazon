5ac308754d9f065fdb7f2af40e0ef43297f309b3
Eric Doppelt <ead45@duke.edu>
2021-10-30 17:56:21 -0400

    Making progress towards connecting UI to backend

--- app/models/product.py
+++ app/models/product.py
@@ -1,93 +1,24 @@
 from flask import current_app as app
 
-import productdisplaypage;
-
 class Product:
-    def __init__(self, product_id, owner_id, description, name, image, category):
-        self.product_id = product_id
-        self.owner_id = owner_id
-        self.description = description
-        self.name = name
-        self.image = image
-        self.category = category
+    def __init__(self, id):
+        self.id = id
 
     @staticmethod
-    def get(product_id):
+    def get(id):
         rows = app.db.execute('''
-SELECT product_id, owner_id, description, name, image, category
-FROM Products
-WHERE product_id = :product_id
+SELECT product_id
+FROM Product
+WHERE id = :id
 ''',
-                              product_id=product_id)
+                              id=id)
         return Product(*(rows[0])) if rows is not None else None
 
     @staticmethod
-    ##method to get all products
-    def get_all():
+    def get_all(available=True):
         rows = app.db.execute('''
-SELECT product_id, owner_id, description, name, image, category
+SELECT product_id
 FROM Product
-''')
+'''
+                              )
         return [Product(*row) for row in rows]
-
-    @staticmethod
-    ##method to return all products based on a certain search criteria
-    def get_products_based_on_search_criteria(search_criteria):
-        rows = app.db.execute('''
-SELECT product_id, owner_id, description, name, image, category
-FROM Product
-WHERE search_criteria = :search_criteria
-AND name LIKE search_criteria OR description LIKE search_criteria
-        
-        ''', search_criteria=search_criteria)
-        return [Product(*row) for row in rows] if rows is not None else None
-
-    @staticmethod
-    ##method to return all products in a certain category
-    def get_products_based_on_category(category):
-        rows = app.db.execute('''
-SELECT product_id, owner_id, description, name, image, category
-FROM Products
-WHERE category = :category
-        ''', category=category)
-        return [Product(*row) for row in rows] if rows is not None else None
-
-    @staticmethod
-    ##A detailed product page will show all details for the product,
-    ##together with a list of sellers and their current quantities in stock
-    def get_product_display_page(product_id):
-        rows = app.db.execute('''
-SELECT p.product_id, p.owner_id, p.description, p.name, p.image, p.category, sp.price, COUNT(si.item_id)
-FROM Products p, SellsProduct sp, SellsItem si
-WHERE product_id = :product_id
-AND p.product_id = sp.product_id
-AND p.owner_id = sp.seller_id
-AND p.product_id = si.product_id
-AND p.owner_id = si.seller_id
-''', product_id=product_id)
-        return [productdisplaypage(*row) for row in rows] if rows is not None else None
-
-    @staticmethod
-    ##Method to insert a new product into the database
-    def insert_new_product(product_id, owner_id, description, name, image, category):
-        insert_statement = '''
-INSERT INTO Product (product_id, owner_id, description, name, image, category)
-VALUES (%s, %s, %s, %s, %s, %s)
-        '''
-        values = (product_id, owner_id, description, name, image, category)
-        try:
-            app.db.execute(insert_statement, values)
-        except Exception as e:
-            print(e)
-
-    @staticmethod
-    ##Method to delete a product from the database
-    def delete_product(product_id):
-        try:
-            app.db.execute('''
-DELETE FROM Product WHERE product_id = :product_id
-        ''', product_id=product_id)
-        except Exception as e:
-            print(e)
-
-
--- app/models/productdisplaypage.py
+++ app/models/productdisplaypage.py
@@ -1,11 +1,11 @@
-class ProductDisplayPage:
+# class ProductDisplayPage:
 
-    def __init__(self, product_id, owner_id, description, name, image, category, price, quantity):
-        self.product_id = product_id
-        self.owner_id = owner_id
-        self.description = description
-        self.name = name
-        self.image = image
-        self.category = category
-        self.price = price
-        self.quantity = quantity
+#     def __init__(self, product_id, owner_id, description, name, image, category, price, quantity):
+#         self.product_id = product_id
+#         self.owner_id = owner_id
+#         self.description = description
+#         self.name = name
+#         self.image = image
+#         self.category = category
+#         self.price = price
+#         self.quantity = quantity
