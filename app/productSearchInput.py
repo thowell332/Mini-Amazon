74357530d49d4c57b2327f0e4c11a0e28d3941c2
Jordan Castleman <jlc153@duke.edu>
2021-11-11 16:35:52 -0500

    minor product search input changes

--- app/productSearchInput.py
+++ app/productSearchInput.py
@@ -7,6 +7,6 @@ bp = Blueprint('productSearchInput', __name__)
 
 @bp.route('/productSearchInput')
 def product():
-    product = Product.get_products_based_on_search_criteria("test") ##change "Laptop" to @param input
+    product = Product.get_products_based_on_search_criteria("Laptops") ##change "Laptop" to @param input
     print(product)
     return render_template('productSearch.html', product=product)
