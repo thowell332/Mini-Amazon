13844140e571e035afe580fc87b076ff6436bcbd
Eric Doppelt <ead45@duke.edu>
2021-11-11 21:49:51 -0500

    Added purchase cart button

--- app/models/cart.py
+++ app/models/cart.py
@@ -1,4 +1,18 @@
 from flask import current_app as app
+from datetime import datetime
+
+class CartEntry:
+    def __init__(self, buyer_id, seller_id, product_id, quantity):
+        self.buyer_id = buyer_id
+        self.seller_id = seller_id
+        self.product_id = product_id
+        self.quantity = quantity
+
+class SellsItemEntry:
+    def __init__(self, seller_id, product_id, item_id):
+        self.seller_id = seller_id
+        self.product_id = product_id
+        self.item_id = item_id
 
 # TODO: Convert from normal query to procedure.
 class CartItem:
@@ -10,6 +24,7 @@ class CartItem:
         self.unit_price = unit_price
         self.total_price = self.unit_price * self.quantity
 
+# TODO: Refactor SQL queries to remove overlap.
 class Cart:
     def __init__(self, product_id, seller_id, quantity):
         self.product_id = product_id
@@ -17,7 +32,7 @@ class Cart:
         self.quantity = quantity
 
     @staticmethod
-    def get_by_buyer_id(buyer_id):
+    def get_cart_for_buyer_id(buyer_id):
         rows = app.db.execute("""
 WITH
 CartInfo(product_id, seller_id, quantity) AS (
@@ -54,8 +69,97 @@ Select * FROM CartInfoProduct
 
 buyer_id = buyer_id)
 
-        print(rows)
         if not rows:  # email not found
-            return None
+            return []
         else:
             return [CartItem(*row) for row in rows]
+
+
+    # TODO: Make this serializable.
+    @staticmethod
+    def purchase_cart_for_buyer_id(buyer_id):
+        cart_tuples = app.db.execute(
+"""
+SELECT *
+FROM Cart
+WHERE buyer_id = :buyer_id
+""",
+buyer_id = buyer_id)
+
+        print(cart_tuples)
+        cart_items = [CartEntry(*cart_tuple) for cart_tuple in cart_tuples]
+
+        out_of_stock_items = []
+        in_stock_items = []
+
+        for item in cart_items:
+            quantity_in_inventory = app.db.execute(
+"""
+SELECT COUNT(*)
+FROM SellsItem
+WHERE product_id = :product_id AND seller_id = :seller_id
+""",
+product_id = item.product_id, seller_id = item.seller_id)
+
+            quantity_in_inventory = quantity_in_inventory[0][0]
+
+            if item.quantity > quantity_in_inventory:
+                out_of_stock_items.append(item)
+            else:
+                in_stock_items.append(item)
+            
+            # Purchase all the in stock items and remove them form cart.
+
+            # TODO: Make purchase ID unique.
+            purchase_id = 123
+            initial_status = 'Unfulfilled'
+
+            for item in in_stock_items:
+
+                # Remove product from Cart table.
+                app.db.execute(
+"""
+DELETE
+FROM Cart
+WHERE product_id = :product_id AND seller_id = :seller_id
+RETURNING 0
+""",
+
+product_id = item.product_id, seller_id = item.seller_id)
+
+                # Remove item from SellsItem table.
+                items_available_for_purchase = app.db.execute(
+"""
+SELECT *
+FROM SellsItem
+WHERE product_id = :product_id AND seller_id = :seller_id
+""",
+product_id = item.product_id, seller_id = item.seller_id)
+
+                items_avaiable_for_purchase = [SellsItemEntry(*item_available) for item_available in items_available_for_purchase]
+
+                # Buy one item for each incremental quantity selected.
+                for i in range(item.quantity):
+                    purchased_item = items_avaiable_for_purchase[i]
+
+                    # Remove the item as being for sale.
+                    app.db.execute(
+"""
+DELETE
+FROM SellsItem
+WHERE item_id = :item_id
+RETURNING 0
+""",
+item_id = purchased_item.item_id)
+
+                    # Note that the item was purchased.
+                    app.db.execute(
+                    """
+                    INSERT INTO Purchase (buyer_id, product_id, item_id, purchase_id, status, date)
+                    VALUES (:buyer_id, :product_id, :item_id, :purchase_id, :status, :date)
+                    RETURNING 0
+                    """,
+                    buyer_id = buyer_id, product_id = purchased_item.product_id, item_id = purchased_item.item_id, purchase_id = purchase_id, status = initial_status, date = datetime.now())
+
+
+
--- db/create.sql
+++ db/create.sql
@@ -50,7 +50,6 @@ purchase_id INTEGER NOT NULL,
 status VARCHAR(32) NOT NULL,
 date TIMESTAMP WITH TIME ZONE NOT NULL,
 PRIMARY KEY (buyer_id, product_id, item_id),
-FOREIGN KEY(product_id, item_id) REFERENCES SellsItem(product_id, item_id)
 );
  
 CREATE TABLE SellerReview
