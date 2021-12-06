c94ae668c02c80622ecec8f4e3cfe72f7c635538
Jordan Castleman <jlc153@duke.edu>
2021-12-06 15:19:35 -0500

    added pagination support

--- app/productSearchCategory.py
+++ app/productSearchCategory.py
@@ -1,12 +1,21 @@
-from flask import render_template
+from flask import render_template, request
+from flask_paginate import Pagination, get_page_parameter
 
 from .models.product import Product
 
 from flask import Blueprint
 bp = Blueprint('productSearchCategory', __name__)
 
+per_page = 10
+
 @bp.route('/productSearchCategory')
 def product():
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * per_page
     product = Product.get_products_based_on_category("Laptops") ##change "Laptop" to @param input
-    print(product)
-    return render_template('productSearch.html', product=product)
+    pagination = Pagination(page=page, per_page=per_page, total=len(product), search=search, record_name='products')
+    return render_template('productSearch.html', product=product[start: start + per_page], pagination=pagination)
--- app/productSearchInput.py
+++ app/productSearchInput.py
@@ -1,12 +1,20 @@
-from flask import render_template
+from flask import render_template, request
+from flask_paginate import Pagination, get_page_parameter
+from flask import Blueprint
 
 from .models.product import Product
 
-from flask import Blueprint
 bp = Blueprint('productSearchInput', __name__)
+per_page = 10
 
 @bp.route('/productSearchInput')
 def product():
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * per_page
     product = Product.get_products_based_on_search_criteria("Laptops") ##change "Laptop" to @param input
-    print(product)
-    return render_template('productSearch.html', product=product)
+    pagination = Pagination(page=page, per_page=per_page, total=len(product), search=search, record_name='products')
+    return render_template('productSearch.html', product=product[start: start + per_page], pagination=pagination)
