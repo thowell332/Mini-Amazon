1e54776853c6852d62e3b14301f117f018e3a979
Jordan Castleman <jlc153@duke.edu>
2021-12-06 15:33:48 -0500

    added the view product functionality

--- app/product.py
+++ app/product.py
@@ -5,8 +5,8 @@ from .models.product import Product
 from flask import Blueprint
 bp = Blueprint('product', __name__)
 
-@bp.route('/product')
-def product():
-    product = Product.get_product_display_page(1) ##replace 1 with @param id
+@bp.route('/product<input>')
+def product(input):
+    product = Product.get_product_display_page(input) ##replace 1 with @param id
     print(product)
     return render_template('product.html', product=product)
