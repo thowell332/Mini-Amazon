027df1f187ec43a6d0c2a98bb3074b2abc38ff9e
Thomas Howell <tjh48@duke.edu>
2021-12-07 18:02:11 -0500

    Finalize order fulfillment pages

--- app/models/orderFulfillment.py
+++ app/models/orderFulfillment.py
@@ -26,7 +26,7 @@ class OrderHistory:
     
     @staticmethod
     # retrieve individual order fulfillment details
-    def get_order_fulfillment(purchase_id):
+    def get_purchase(purchase_id):
         row = app.db.execute(
             '''
             SELECT p.purchase_id, CONCAT(a.firstname,' ',a.lastname) as buyer_name,
@@ -37,6 +37,79 @@ class OrderHistory:
             ''',
             purchase_id=purchase_id
         )
-        print(row[0])
         return OrderHistory(*(row[0])) if row else None
 
+
+class OrderFulfillment:
+    def __init__(self, product_id, product_name, product_description, quantity, status):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.product_description = product_description
+        self.quantity = quantity
+        self.status = status
+    
+    @staticmethod
+    # retrieve individual order fulfillment details
+    def get_order_fulfillment(purchase_id):
+        rows = app.db.execute(
+            '''
+            SELECT pr.product_id, pr.name as product_name, pr.description as product_description,
+            COUNT(pu.item_id) as quantity, MIN(pu.status) as status
+            FROM Purchase pu, Product pr
+            WHERE pu.purchase_id = :purchase_id AND pr.product_id = pu.product_id
+            GROUP BY pr.product_id, pr.name, pr.description;
+            ''',
+            purchase_id=purchase_id
+        )
+        return [OrderFulfillment(*row) for row in rows] if rows is not None else None
+
+
+class ItemFulfillment:
+    def __init__(self, item_id, status):
+        self.item_id = item_id
+        self.status = status
+    
+    @staticmethod
+    # retrieve individual item fulfillment details
+    def get_item_fulfillment(purchase_id, product_id):
+        rows = app.db.execute(
+            '''
+            SELECT p.item_id, p.status
+            FROM Purchase p
+            WHERE p.purchase_id = :purchase_id AND p.product_id = :product_id;
+            ''',
+            purchase_id=purchase_id,
+            product_id=product_id
+        )
+        return [ItemFulfillment(*row) for row in rows] if rows is not None else None
+    
+    @staticmethod
+    # update statuses
+    def update_status(purchase_id, product_id, item_id, status):
+        print(item_id)
+        print(status)
+        # update the statuses of all items of same product if item_id = -1
+        if item_id == -1:
+            try: app.db.execute(
+                '''
+                UPDATE Purchase SET status = :status
+                WHERE purchase_id = :purchase_id AND product_id = :product_id;
+                ''',
+                purchase_id=purchase_id,
+                product_id=product_id,
+                status=status
+            )
+            except Exception as e:
+                print(e)
+        # else update status of individual item
+        else:
+            try: app.db.execute(
+                '''
+                UPDATE Purchase SET status = :status
+                WHERE item_id = :item_id;
+                ''',
+                item_id=item_id,
+                status=status
+            )
+            except Exception as e:
+                print(e)
--- app/orderFulfillments.py
+++ app/orderFulfillments.py
@@ -4,38 +4,50 @@ from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.fields.core import DateField, DecimalField, IntegerField
+from wtforms.fields.core import DateField, DecimalField, IntegerField, SelectField
 from wtforms.validators import DataRequired, NumberRange, Optional
 from flask_babel import _, lazy_gettext as _l
 
-from .models.orderFulfillment import OrderHistory
+from .models.orderFulfillment import ItemFulfillment, OrderFulfillment, OrderHistory
+from .models.product import Product
 
 from flask import Blueprint
 bp = Blueprint('orderFulfillments', __name__)
 
 @bp.route('/orderFulfillments')
-def inventory():
+def orderFulfillments():
     # get order history
     orderHistory = OrderHistory.get('2') # CHANGE '2' TO USER ID
     # render the page by adding information to the index.html file
     return render_template('orderFulfillments.html', orderHistory=orderHistory)
 
-class EditOrderForm(FlaskForm):
-    orderID = IntegerField(_l('Order ID'), validators=[Optional()])
-    buyerName = StringField(_l('Buyer Name'), validators=[Optional()])
-    buyerAddress = StringField(_l('Buyer Address'), validators=[Optional()])
-    quantity = IntegerField(_l('Item Quantity'), validators=[Optional()])
-    date = DateField(_l('Date'), validators=[Optional()])
-    status = StringField('Status', validators=[DataRequired()])
-    submit = SubmitField(_l('Submit'))
-
 @bp.route('/orderFulfillmentDetails/<int:purchase_id>', methods=['GET', 'POST'])
 def orderFulfillmentDetails(purchase_id):
-    purchase = OrderHistory.get_order_fulfillment(purchase_id)
-    form = EditOrderForm()
-    if form.validate_on_submit():
-        flash('Product listing has been updated')
-        return redirect(url_for('inventories.inventory'))
+    purchase = OrderHistory.get_purchase(purchase_id)
+    fulfillment = OrderFulfillment.get_order_fulfillment(purchase_id)
     # render the page by adding information to the index.html file
-    return render_template('editOrderFulfillment.html', title='View/Update Order Details', form=form, purchase=purchase)
+    return render_template('orderFulfillmentDetails.html', purchase=purchase, fulfillment=fulfillment)
+
+class EditOrderForm(FlaskForm):
+    item = SelectField(_l('Item ID'), coerce=int, validators=[DataRequired()])
+    status = SelectField('Status', coerce=int, validators=[DataRequired()])
+    submit = SubmitField(_l('Submit'))
+
+@bp.route('/orderFulfillmentDetails/<int:purchase_id>/<int:product_id>', methods=['GET', 'POST'])
+def editOrderFulfillment(purchase_id, product_id):
+    purchase = OrderHistory.get_purchase(purchase_id)
+    product = Product.get(product_id)
+    # get item fulfillment statuses
+    itemFulfillment = ItemFulfillment.get_item_fulfillment(purchase_id, product_id)
+    itemList = [item.item_id for item in itemFulfillment]
+    # create edit fulfillment form
+    form = EditOrderForm()
+    form.item.choices = [(-1, 'All Items')] + [(id, id) for id in itemList]
+    form.status.choices = [(1, 'ORDERED'), (2, 'SHIPPED'), (3, 'DELIVERED')]
+    if form.validate_on_submit():
+        ItemFulfillment.update_status(purchase_id, product_id, form.item.data, form.status.data - 1)
+        flash('Item fulfillment(s) has been updated')
+        itemFulfillment = ItemFulfillment.get_item_fulfillment(purchase_id, product_id)
+    # render the page by adding information to the index.html file
+    return render_template('editOrderFulfillment.html', purchase=purchase, product=product, form=form, itemFulfillment=itemFulfillment)
 
